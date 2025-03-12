421abc96b0ce97ce418cb0fce664870ff5b291a9
ajlouni77 <ajlouni702@gmail.com>
2025-03-13 01:39:23 +0300

    contact

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
+import ContactUs from "./pages/contact";
+
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-      </Routes>
-    </Router>
- </> 
- );
+      <Router>
+        <Routes>
+          {/* <Route path="/" element={<h1>Home Page</h1>} /> */}
+          <Route path="/" element={<ContactUs />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
