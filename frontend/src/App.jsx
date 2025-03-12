f6b6d5adbaf2ea2afb27e79dcb5f2fc7c4547a81
Bilal-Zaro <bilalza2004@gmail.com>
2025-03-13 00:13:30 +0300

    the models setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
+import ArticlesPage from "./pages/ArticlesPage";
 function App() {
   return (
     <>
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
+        {/* <Route path="" element={<h1>Home Page</h1>} /> */}
+        <Route path="/" element={<ArticlesPage/>} />
       </Routes>
     </Router>
  </> 
