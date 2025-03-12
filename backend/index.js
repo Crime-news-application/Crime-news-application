429e381b4d1fba9e3d3675f9793fcf0ca8362ed9
Bilal-Zaro <bilalza2004@gmail.com>
2025-03-12 19:40:30 +0300

    config the db

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+require("dotenv").config();
 
-dotenv.config();
 
 
 const app = express();
 
+connectDB();
 
 app.use(cors()); // السماح بالوصول من مختلف المصادر
 app.use(express.json()); // لقراءة البيانات بتنسيق JSON من الطلبات
 
-// استيراد المسارات
-const userRouter = require("./routers/userRouter");
-
-// ربط المسارات
-app.use("/api/users", userRouter);
 
 // استماع على المنفذ المحدد
 const PORT = process.env.PORT || 4000;
