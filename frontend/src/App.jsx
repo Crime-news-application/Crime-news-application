0bd058932c5a8bdf82af59dcb698df7a5ac7186b
ahmma <ahmmadtarawnah@gmail.com>
2025-03-12 23:35:18 +0300

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
+import Navbar from "./Component/Navbar";
+
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-      </Routes>
-    </Router>
- </> 
- );
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
