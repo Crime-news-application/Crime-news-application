f21ae3f643fc41c73aefc5a583fae34a3757029e
ahmadalnajjar01 <ahmadalnajjar609@gmail.com>
2025-03-16 00:53:47 +0300

    najjar pull

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -326,27 +326,6 @@ const getUserProfile = async (req, res) => {
 
 
 
-/*
- try {
-    const token = req.cookies.token;
-
-    if (!token) {
-      return res.status(401).json({ message: "No token provided" });
-    }
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.id).select("-password");
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    res.json(user);
-  } catch (err) {
-    res.status(401).json({ message: "Invalid token" });
-  }
-
-*/ 
 
 
 
